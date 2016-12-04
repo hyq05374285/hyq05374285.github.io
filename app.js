4af809156ef6331e454b997c4dcca5e2d89ebc77
hyq05374285 <419099902@qq.com>
2016-12-04 16:38:27 +0800

    12.4

---
+++ app.js
@@ -0,0 +1,70 @@
+var request = require('request');
+var fs = require('fs');
+var cheerio = require('cheerio');
+var http = require('http');
+var site = 'https://www.zhihu.com';
+
+var options = {
+  method: 'GET',
+  uri: 'https://www.zhihu.com/search',
+  qs: {
+    type: 'content',
+    q: 'jave'
+  }
+};
+// 網址, callback
+var server = http.createServer(function (req, res) {
+    // req 是本地端請求的訊息
+    // res 是主機回傳到本地端的訊息
+    request(options, function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+   // console.log(body) // Show the HTML for the Google homepage.
+    
+      var $ = cheerio.load(body);
+
+    var $title = $('a.js-title-link');
+
+    var result = [];
+
+    for(var i=0; i<$title.length; i++) {
+      var item = {
+        title: $($title[i]).text(),
+        link: site + $($title[i]).attr('href')
+      }
+
+      // var str = $($title[i]).text();
+      // var url = site + $($title[i]).attr('href');
+      console.log(item);
+      // console.log(url);
+
+      result.push(item);
+    }
+
+    var str = JSON.stringify(result, null, 4);
+    // 將result存成 JSON 檔
+
+    fs.writeFile('result.json', str, 'utf8' ,function (err){
+        if (err) {
+            console.log('存档失败');
+            return;
+        }
+        console.log('存档完成');
+    });
+  }
+})
+var filename = __dirname + '\\result.json';
+    fs.readFile(filename, 'utf8',function (err,content){
+        if (err){
+            res.writeHead(404,{'content-type':'text/html'});
+            res.end();
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(content,'utf8');
+    console.log(content);
+    res.end();
+    })
+    
+})
+server.listen(23456);
+
--- http.js
+++ http.js
@@ -3,28 +3,30 @@ var http = require('http');
 // 引用 File System 模組
 var fs = require('fs');
 
-console.log(__dirname);
+var file_content;
 
 var filename = __dirname + '\\index.html';
-console.log(filename);
 
-// 讀取檔案
-fs.readFile(filename,'utf8', function(err, content) {
-    if (err) {
-        console.log('Failed to read');
-        return;
-    }
-var server = http.createServer(function(req, res){
+var server = http.createServer(function (req, res) {
     // req 是本地端請求的訊息
     // res 是主機回傳到本地端的訊息
 
-    // 將檔案內容印出
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(content);
-    res.end();
-})
+    // 讀取檔案
+    fs.readFile(filename, 'utf8', function (err, content) {
+        if (err) {
+            console.log('Failed to read');
+            // 若檔案讀取錯誤，回傳 404
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end();  
+            return;
+        }
+        // 將檔案內容傳給瀏覽器
+        //res.writeHead(200, { 'Content-Type': 'text/' });
+        res.write(content);
+        res.end();
+    })
+});
 
 // 監聽 12345 port
 server.listen(12345);
-});
 console.log('Server running at http://10.10.60.116:12345/');
